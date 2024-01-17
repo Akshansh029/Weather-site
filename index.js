369c90b7ebe2c9a4caa0a89b03d15c1bab637298
akshanshsingh00 <akshanshsingh00@gmail.com>
2024-01-17 21:23:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const apiKey = "c2e1022d0a71a8f69369dfc538456e5b";
+const city = "Chennai";
+
+async function fetchWeatherData(){
+    const urlWithApiKey = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    try{
+        const response = await fetch(urlWithApiKey);
+    if(!response.ok){
+        throw new Error("Could not fetch data");
+    }
+    // console.log(response);
+    const data = await response.json();
+    console.log(data);
+    console.log(data.name)
+    console.log(data.weather[0].description)
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+fetchWeatherData();
